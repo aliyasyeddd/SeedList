12c01a0d8690ab36e68391dc9508005611dc72e3
aliya mahaboob <mahaboobaliya02@gmail.com>
2025-11-01 11:32:43 +0530

    refactoring ux

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 
 let addBtn = document.querySelector(".add-btn")
+let taskInput = document.getElementById("task-input")
+let List = document.querySelector(".task-list")
+let taskList = []
 
 
 addBtn.addEventListener("click", function() {
-    console.log("Button clicked")
+    taskList.push(taskInput.value)
+    console.log(taskInput.value)
+    renderTasks()
+    taskInput.value = ""
 })
+
+function renderTasks() {
+    let taskListHTML = ""
+    for(let tasks of taskList) {
+        taskListHTML += `<li class="task-item">${tasks}</li>`
+    }
+    List.innerHTML = taskListHTML
+}

9a368dbc7c6fd8b6ecd42ba1799f7df3ce3032fe
aliya mahaboob <mahaboobaliya02@gmail.com>
2025-11-02 17:59:47 +0530

    updated features

--- index.js
+++ index.js
@@ -5,17 +5,28 @@ let List = document.querySelector(".task-list")
 let taskList = []
 
 
-addBtn.addEventListener("click", function() {
-    taskList.push(taskInput.value)
-    console.log(taskInput.value)
+addBtn.addEventListener("click", function () {
+    if(taskInput.value) {
+      taskList.push(taskInput.value)
+    }
     renderTasks()
     taskInput.value = ""
 })
 
 function renderTasks() {
     let taskListHTML = ""
-    for(let tasks of taskList) {
-        taskListHTML += `<li class="task-item">${tasks}</li>`
+    for (let tasks of taskList) {
+        taskListHTML += `
+        <li class="task-item"> 
+        <span class="task-text">${tasks}</span>
+        <div class="icon-left">
+          <i class="fa-regular fa-pen-to-square edit-btn"></i> 
+          <i class="fa-solid fa-trash delete-btn"></i>
+        </div>
+      </li>`
     }
     List.innerHTML = taskListHTML
 }
+
+renderTasks()
+

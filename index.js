bf80d63648cea122ec33b06bd870afc52769afae
aliya mahaboob <mahaboobaliya02@gmail.com>
2025-11-05 11:23:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,19 +5,25 @@ let List = document.querySelector(".task-list")
 let taskList = []
 
 
+
+localStorage.clear()
+let tasksFromLocalStorage = JSON.parse(localStorage.getItem("tasklist"))
+
+
 addBtn.addEventListener("click", function () {
-    if(taskInput.value) {
-      taskList.push(taskInput.value)
-    }
-    renderTasks()
+  if (taskInput.value) {
+    taskList.push(taskInput.value)
     taskInput.value = ""
+    localStorage.setItem("tasklist", JSON.stringify(taskList))
+    renderTasks()
+  }
 })
 
 
 function renderTasks() {
-    let taskListHTML = ""
-    for (let tasks of taskList) {
-        taskListHTML += `
+  let taskListHTML = ""
+  for (let tasks of taskList) {
+    taskListHTML += `
         <li class="task-item"> 
         <span class="task-text">${tasks}</span>
         <div class="icon-left">
@@ -25,8 +31,8 @@ function renderTasks() {
           <i class="fa-solid fa-trash delete-btn"></i>
         </div>
       </li>`
-    }
-    List.innerHTML = taskListHTML
+  }
+  List.innerHTML = taskListHTML
 }
 
 renderTasks()
